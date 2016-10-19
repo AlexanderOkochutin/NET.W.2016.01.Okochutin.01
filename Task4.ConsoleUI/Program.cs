090fd30b4a0285e75f04f0d1ba1639adab9f5f71
AlexanderOkochutin <okochutinwork@gmail.com>
2016-10-19 22:59:49 +0300

    Upload to git

---
+++ Task4.ConsoleUI/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Task4.Logic;
+
+namespace Task4.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            do
+            {
+                Console.WriteLine("Enter array of int in next format  -m a0,a1,a2  or -r min,max,count "); // -m manual enter array in next format 1,2,3... or 1 2 3
+                Console.WriteLine("example of input info: -m 1,14,3,8,19 or -r -5,20,10");                  // -r random array in range from min to max
+                Console.WriteLine(new string('*',67));
+                string inputData = Console.ReadLine(); // get input data in format -param a,b,c...
+                Random rand = new Random();
+                int[] rsltArray = null;
+                bool tryAgain = true;
+                string cmdParam = "noParam";
+                if (inputData.Length >= 2)
+                {
+                    cmdParam = inputData.Substring(0, 2);
+                }
+                if (cmdParam == "-m") // manual input array
+                {
+                    while (tryAgain) // in case of fail repeat 
+                    {
+                        try
+                        {
+                            rsltArray = Array.ConvertAll(inputData.Substring(3).Split(new char[] { ' ', ',' }), int.Parse); // parse string[] into int[]
+                            tryAgain = false; // set in case of success
+                        }
+
+                        catch
+                        {
+                            Console.WriteLine("****Please check input information, and try again!****");
+                            Console.WriteLine("****example of input info: -m 1,2,3  or  -m 1 2 3 ****");
+                            inputData = Console.ReadLine();
+                        }
+                    }
+                }
+                if (cmdParam == "-r") // random input array
+                {
+                    while (tryAgain) // in case of fail repeat
+                    {
+                        try
+                        {
+                            int[] randParam = Array.ConvertAll(inputData.Substring(3).Split(new char[] { ' ', ',' }), int.Parse); // parse string[] into int[]
+                            if (randParam[2] <= 0)
+                            {
+                                throw new Exception();
+                            }
+
+                            rsltArray = new int[randParam[2]];
+                            for (int i = 0; i < randParam[2]; i++)
+                            {
+                                rsltArray[i] = rand.Next((int)randParam[0], (int)randParam[1]);
+                                Console.Write(rsltArray[i].ToString() + ",");
+                            }
+                            Console.WriteLine();
+                            tryAgain = false; // set in case of success
+                        }
+
+                        catch
+                        {
+                            Console.WriteLine("****Please check input information, and try again!****");
+                            Console.WriteLine("****example of input info: -r min,max,count  or  -r min max count ****");
+                            inputData = Console.ReadLine();
+                        }
+                    }
+                }
+
+                if (tryAgain == false)
+                {
+                    Console.WriteLine("enter -i for increase sort or -d for decrease sort");
+                    string sortType = Console.ReadLine();
+                    if (sortType == "-i")
+                    {
+                        rsltArray.MergeSort(new ExtensionToolsForArray.IncComparer<int>());
+                        Console.WriteLine("Sorted array");
+                    }
+
+                    else if (sortType == "-d")
+                    {
+                        rsltArray.MergeSort(new ExtensionToolsForArray.DecComparer<int>());
+                        Console.WriteLine("Sorted array");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not correct sort_type");
+                    }
+                    
+                    foreach (int i in rsltArray)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("check input information!");
+                }
+                Console.WriteLine("enter 'stop' to exit program or press 'enter' to continue");
+
+            } while (Console.ReadLine() != "stop");
+        }
+    }
+    
+}
---
+++ Task4/ExtensionToolsForArray.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Logic
+{
+    public static class ExtensionToolsForArray
+    {
+        public static void MergeSort<T>(this T[] array, IComparer<T> comparer)
+        {
+            MergeSort<T>(array, 0, array.Length - 1, comparer);
+        }
+
+        private static void MergeSort<T>(T[] array, int left, int right, IComparer<T> comparer)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                MergeSort<T>(array, left, middle, comparer);
+                MergeSort<T>(array, middle + 1, right, comparer);
+                Merge<T>(array, left, right, comparer);
+            }
+        }
+
+        private static void Merge<T>(T[] array, int left, int right, IComparer<T> comparer)
+        {
+            T[] buff = new T[right - left + 1];
+            int middle = left + (right - left) / 2;
+            int ind1 = left, ind2 = middle + 1, indBuff = 0;
+            while (ind1 <= middle && ind2 <= right)
+            {
+                if (comparer.Compare(array[ind1], array[ind2]) <= 0)
+                {
+                    buff[indBuff++] = array[ind1++];
+                }
+                else
+                {
+                    buff[indBuff++] = array[ind2++];
+                }
+            }
+
+            while (ind1 <= middle)
+            {
+                buff[indBuff++] = array[ind1++];
+            }
+
+            while (ind2 <= right)
+            {
+                buff[indBuff++] = array[ind2++];
+            }
+
+            for (int i = left; i <= right; i++)
+            {
+                array[i] = buff[i - left];
+            }
+        }
+
+        public class IncComparer<T> : IComparer<T> where T : IComparable<T>
+        {
+            public int Compare(T x, T y)
+            {
+                if (x.CompareTo(y) > 0) { return 1; }
+                else if (x.CompareTo(y) < 0)
+                { return -1; }
+                else { return 0; }
+            }
+        }
+
+        public class DecComparer<T> : IComparer<T> where T : IComparable<T>
+        {
+            public int Compare(T x, T y)
+            {
+                if (x.CompareTo(y) < 0) { return 1; }
+                else if (x.CompareTo(y) > 0)
+                { return -1; }
+                else { return 0; }
+            }
+        }
+    }
+   
+}
+
+
