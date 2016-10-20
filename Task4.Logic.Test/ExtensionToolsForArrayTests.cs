8576f531bd706fd6f4ca3c094f3c2b4a24699cd2
AlexanderOkochutin <okochutinwork@gmail.com>
2016-10-20 10:58:22 +0300

    Simple unit test

--- Task4.Logic.Test/ExtensionToolsForArrayTests.cs
+++ Task4.Logic.Test/ExtensionToolsForArrayTests.cs
@@ -11,25 +11,22 @@ namespace Task4.Logic.Test
         public void TestMethod1()
         {
             int[] testArray0 = new int[50];
+            int[] testArray1 = new int[50];
             int[] rstExpected0 = new int[50];
             Random rand = new Random();
             for (int i = 0; i < 50; i++)
             {
                 int a = rand.Next(-100, 100);
                 testArray0[i] = a;
+                testArray1[i] = a;
                 rstExpected0[i] = a;
             }
             Array.Sort(rstExpected0);
             testArray0.MergeSort(new ExtensionToolsForArray.IncComparer<int>());
-            int[] testArray1 = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-            int[] testArray2 = new int[] { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0 };
-            int[] rstExpected1 = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int[] rstExpected2 = new int[] { 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 };
-            testArray1.MergeSort(new ExtensionToolsForArray.IncComparer<int>());
-            testArray2.MergeSort(new ExtensionToolsForArray.DecComparer<int>());
-            CollectionAssert.AreEqual(rstExpected2, testArray2);
-            CollectionAssert.AreEqual(rstExpected1,testArray1);
             CollectionAssert.AreEqual(rstExpected0, testArray0);
+            Array.Reverse(rstExpected0);
+            testArray1.MergeSort(new ExtensionToolsForArray.DecComparer<int>());
+            CollectionAssert.AreEqual(rstExpected0, testArray1);
         }
 
     }
